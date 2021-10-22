588ba796f3dc20ffafe2cf4bdf9430fbe146b815
AniqAli <Aneeqkhananeeq99@gmail.com>
2021-10-22 16:18:20 +0500

    createSalVoucher done in payroll in accounts

--- src/components/utils/CustomTable.jsx
+++ src/components/utils/CustomTable.jsx
@@ -1,173 +1,174 @@
-import React, { useState } from 'react';
-import Loader from 'react-loader-spinner';
-import Button from './Button';
-import moment from 'moment';
+import React, { useState } from "react";
+import Loader from "react-loader-spinner";
+import Button from "./Button";
+import moment from "moment";
+import { capitalize } from "../../utils/capitalize";
 
 function generateRow(data, keys) {
-	const values = [];
-	for (const key of keys) {
-		let value = { ...data };
-		if (typeof key === 'object') {
-			if (key.indexFrom) {
-				value = key.indexFrom[value[key.keyName]];
-				values.push(value);
-			} else if (key.dateFrom) {
-				value = moment(value[key.dateFrom]).format('DD MMMM YYYY');
-				values.push(value);
-			}
-		} else {
-			const keyArr = key.split('.');
-			for (const el of keyArr) {
-				if (value[el]) value = value[el];
-				else value = '';
-			}
-			values.push(value);
-		}
-	}
-	return values;
+  const values = [];
+  for (const key of keys) {
+    let value = { ...data };
+    if (typeof key === "object") {
+      if (key.indexFrom) {
+        value = key.indexFrom[value[key.keyName]];
+        values.push(value);
+      } else if (key.dateFrom) {
+        value = moment(value[key.dateFrom]).format("DD MMMM YYYY");
+        values.push(value);
+      }
+    } else {
+      const keyArr = key.toString().split(".");
+      for (const el of keyArr) {
+        if (value[el]) value = value[el];
+        else value = "";
+      }
+      values.push(capitalize(value));
+    }
+  }
+  return values;
 }
 
 const CustomTable = ({
-	tableStyle,
-	heading,
-	columnHeadings,
-	data = [],
-	keys,
-	onFirstOptionClick,
-	firstOptionText,
-	onSecondOptionClick,
-	secondOptionText,
-	withSrNo,
-	fetchLoading,
-	thirdOptionText,
-	onThirdOptionClick,
-	tablePrint,
+  tableStyle,
+  heading,
+  columnHeadings,
+  data = [],
+  keys,
+  onFirstOptionClick,
+  firstOptionText,
+  onSecondOptionClick,
+  secondOptionText,
+  withSrNo,
+  fetchLoading,
+  thirdOptionText,
+  onThirdOptionClick,
+  tablePrint,
 }) => {
-	function onClick(e) {
-		const index = e.target.dataset.option;
-		switch (index) {
-			case '0':
-				onFirstOptionClick(JSON.parse(e.target.dataset.element));
-				return;
-			case '1':
-				onSecondOptionClick(JSON.parse(e.target.dataset.element));
-				return;
-			case '2':
-				onThirdOptionClick(JSON.parse(e.target.dataset.element));
-				return;
-			default:
-				return;
-		}
-	}
+  function onClick(e) {
+    const index = e.target.dataset.option;
+    switch (index) {
+      case "0":
+        onFirstOptionClick(JSON.parse(e.target.dataset.element));
+        return;
+      case "1":
+        onSecondOptionClick(JSON.parse(e.target.dataset.element));
+        return;
+      case "2":
+        onThirdOptionClick(JSON.parse(e.target.dataset.element));
+        return;
+      default:
+        return;
+    }
+  }
 
-	return fetchLoading ? (
-		<div
-			style={{
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				marginTop: '3rem',
-			}}
-		>
-			<Loader type="TailSpin" color="#000" width="3rem" height="3rem" />
-		</div>
-	) : data.length === 0 ? (
-		<p>There is no data found.</p>
-	) : (
-		<>
-			<table class="table table-responsive table-hover table-striped table-bordered border-dark text-center mt-5">
-				<thead
-					class={`${tablePrint ? '' : 'bg-dark'} ${
-						tablePrint ? 'text-dark' : 'text-light'
-					}`}
-				>
-					<tr>
-						{columnHeadings.map((el) => (
-							<th align="center">{el}</th>
-						))}
-						{(onFirstOptionClick ||
-							onSecondOptionClick ||
-							onThirdOptionClick) && <th align="center">Actions</th>}
-					</tr>
-				</thead>
-				<tbody>
-					{data.map((el, i) => (
-						<tr>
-							{withSrNo && (
-								<td className="text-dark bg-light" align="center">
-									{i + 1}
-								</td>
-							)}
-							{generateRow(el, keys).map((el) => (
-								<td className="text-dark bg-light" align="center">
-									{el ? el : 'Not found'}
-								</td>
-							))}
-							{(onFirstOptionClick ||
-								onSecondOptionClick ||
-								onThirdOptionClick) && (
-								<td className="text-dark bg-light" align="center">
-									<div
-										style={{
-											display: 'flex',
-											alignItems: 'center',
-											justifyContent: 'center',
-										}}
-									>
-										{onFirstOptionClick && (
-											<Button
-												variant="contained"
-												classNames="bg-dark text-light"
-												size="small"
-												onClick={onClick}
-												text={firstOptionText}
-												style={{ marginTop: 2 }}
-												btnProps={{
-													'data-element': JSON.stringify(el),
-													'data-option': '0',
-												}}
-											/>
-										)}
+  return fetchLoading ? (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "3rem",
+      }}
+    >
+      <Loader type="TailSpin" color="#000" width="3rem" height="3rem" />
+    </div>
+  ) : data.length === 0 ? (
+    <p>There is no data found.</p>
+  ) : (
+    <>
+      <table class="table table-responsive table-hover table-striped table-bordered border-dark text-center mt-5">
+        <thead
+          class={`${tablePrint ? "" : "bg-dark"} ${
+            tablePrint ? "text-dark" : "text-light"
+          }`}
+        >
+          <tr>
+            {columnHeadings.map((el) => (
+              <th align="center">{el}</th>
+            ))}
+            {(onFirstOptionClick ||
+              onSecondOptionClick ||
+              onThirdOptionClick) && <th align="center">Actions</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((el, i) => (
+            <tr>
+              {withSrNo && (
+                <td className="text-dark bg-light" align="center">
+                  {i + 1}
+                </td>
+              )}
+              {generateRow(el, keys).map((el) => (
+                <td className="text-dark bg-light" align="center">
+                  {el ? el : "Not found"}
+                </td>
+              ))}
+              {(onFirstOptionClick ||
+                onSecondOptionClick ||
+                onThirdOptionClick) && (
+                <td className="text-dark bg-light" align="center">
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {onFirstOptionClick && (
+                      <Button
+                        variant="contained"
+                        classNames="bg-dark text-light"
+                        size="small"
+                        onClick={onClick}
+                        text={firstOptionText}
+                        style={{ marginTop: 2 }}
+                        btnProps={{
+                          "data-element": JSON.stringify(el),
+                          "data-option": "0",
+                        }}
+                      />
+                    )}
 
-										{onSecondOptionClick && (
-											<Button
-												variant="contained"
-												color="secondary"
-												size="small"
-												text={secondOptionText}
-												style={{ marginLeft: 2, marginTop: 2 }}
-												btnProps={{
-													'data-element': JSON.stringify(el),
-													'data-option': '1',
-												}}
-												onClick={onClick}
-											/>
-										)}
+                    {onSecondOptionClick && (
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                        text={secondOptionText}
+                        style={{ marginLeft: 2, marginTop: 2 }}
+                        btnProps={{
+                          "data-element": JSON.stringify(el),
+                          "data-option": "1",
+                        }}
+                        onClick={onClick}
+                      />
+                    )}
 
-										{onThirdOptionClick && (
-											<Button
-												variant="contained"
-												color="secondary"
-												size="small"
-												classNames="bg-dark text-light"
-												text={thirdOptionText}
-												style={{ marginLeft: 2, marginTop: 2 }}
-												btnProps={{
-													'data-element': JSON.stringify(el),
-													'data-option': '2',
-												}}
-												onClick={onClick}
-											/>
-										)}
-									</div>
-								</td>
-							)}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</>
-	);
+                    {onThirdOptionClick && (
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        size="small"
+                        classNames="bg-dark text-light"
+                        text={thirdOptionText}
+                        style={{ marginLeft: 2, marginTop: 2 }}
+                        btnProps={{
+                          "data-element": JSON.stringify(el),
+                          "data-option": "2",
+                        }}
+                        onClick={onClick}
+                      />
+                    )}
+                  </div>
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default CustomTable;
--- src/dashboard/Accounts/payroll/CreatePayroll.jsx
+++ src/dashboard/Accounts/payroll/CreatePayroll.jsx
@@ -1,33 +1,133 @@
-import React, { useEffect } from 'react';
-
-import { getEmployees } from '../../../services/action/EmployeesAction';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { CustomInput, CustomButton } from '../../../components';
-import Sidenav from '../../SideNav/Sidenav';
+import React, { useEffect, useState } from "react";
+import { getEmployees } from "../../../services/action/EmployeesAction";
+import { useDispatch, useSelector } from "react-redux";
+import { CustomInput, CustomButton, CustomTable } from "../../../components";
+import Sidenav from "../../SideNav/Sidenav";
+import Checkbox from "@material-ui/core/Checkbox";
 
 const CreatePayroll = () => {
-	const { employees } = useSelector((state) => state.employees);
+  const [fetchLoading, setFetchLoading] = useState(true);
+  const [fetchError, setFetchError] = useState();
 
-	const dispatch = useDispatch();
+  const { employees } = useSelector((state) => state.employees);
 
-	useEffect(() => {
-		dispatch(getEmployees());
-	}, []);
+  const dispatch = useDispatch();
 
-	console.log(employees);
+  useEffect(() => {
+    setFetchLoading(true);
+    dispatch(
+      getEmployees(null, (err) => {
+        if (err) {
+          setFetchError(err);
+          setTimeout(() => {
+            setFetchError("");
+          }, 4000);
+        }
+        setFetchLoading(false);
+      })
+    );
+  }, []);
 
-	return (
-		<Sidenav title="Create Payroll">
-			<div>
-				{employees.map((el) => (
-					<div>
-						<p>{el.name}</p>
-					</div>
-				))}
-			</div>
-		</Sidenav>
-	);
+  //   console.log(employees);
+
+  return (
+    <Sidenav title="Create Payroll">
+      <div>
+        <div className="d-flex justify-content-between">
+          <div
+            style={{
+              display: "flex",
+              // justifyContent: "center",
+              // alignItems: "center",
+            }}
+          >
+            <p className="mt-3">Check All</p>
+            <Checkbox color="default" />
+          </div>
+          <CustomButton text="Pay" classNames="btn btn-sm bg-dark text-light" />
+        </div>
+        <table class="table table-responsive table-hover table-striped table-bordered border-dark text-center mt-2">
+          <thead class="thead-inverse bg-dark text-light">
+            <tr>
+              <td>Approve</td>
+              <td>Sr. #</td>
+              <td>Employee Name</td>
+              <td>Designation</td>
+              <td>Department</td>
+              <td>Salary</td>
+              <td>Leaves</td>
+              <td>Deduction</td>
+              <td>Final Salary</td>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((emp, i) => {
+              //   console.log(emp);
+              return (
+                <tr>
+                  <td>
+                    <Checkbox color="default" size="small" />
+                  </td>
+                  <td>{i + 1}</td>
+                  <td>{emp.name}</td>
+                  <td>{emp.finalDesignation.name}</td>
+                  <td>{emp.finalDepartment.name}</td>
+                  <td>{emp.finalSal}</td>
+                  <td>{}-</td>
+                  <td>{}-</td>
+                  <td>{}-</td>
+                </tr>
+              );
+            })}
+            <tr>
+              <td colspan="8" style={{ textAlign: 'right', fontWeight: 'bold' }}>Total Salary Amount: </td>
+              <td>{}12,12100</td>
+            </tr>
+            <tr>
+              <td colspan="8" style={{ textAlign: 'right', fontWeight: 'bold' }}>Total Deduction Amount: </td>
+              <td>{}12,12100</td>
+            </tr>
+            <tr>
+              <td colspan="8" style={{ textAlign: 'right', fontWeight: 'bold' }}>Final Salary Amount: </td>
+              <td>{}12,12100</td>
+            </tr>
+          </tbody>
+        </table>
+		<CustomButton text="Create Salary Voucher" classNames="btn btn-sm bg-dark text-light" style={{ float: 'right' }} />
+        {/* <CustomTable
+          fetchLoading={fetchLoading}
+          data={employees}
+          columnHeadings={[
+            "Sr.#",
+            "Employee Name",
+            "Designation",
+            "Department",
+            "Salary",
+            "Leaves",
+            "Deduction",
+            "Final Salary",
+            "Approve",
+          ]}
+          keys={[
+            "name",
+            "finalDesignation.name",
+            "finalDepartment.name",
+            "finalSal",
+            "",
+            "",
+            "",
+            "",
+          ]}
+          withSrNo
+        /> */}
+        {/* {employees.map((el) => (
+          <div>
+            <p>{el.name}</p>
+          </div>
+        ))} */}
+      </div>
+    </Sidenav>
+  );
 };
 
 export default CreatePayroll;
