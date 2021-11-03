7aeb2ad82924bc34266f39d2c02f21bd365899de
AniqAli <Aneeqkhananeeq99@gmail.com>
2021-11-03 14:07:45 +0500

    .

--- src/dashboard/marketing-sales/customer complaint/PrintCustomerComplaintForm.jsx
+++ src/dashboard/marketing-sales/customer complaint/PrintCustomerComplaintForm.jsx
@@ -38,85 +38,276 @@ const PrintCustomerComplaintForm = () => {
           <p style={{ textDecoration: "underline" }}>{}12-12-12</p>
         </div>
       </div>
-      <div style={{ marginTop: "4rem", borderTop: "1px solid black" }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            marginTop: "1rem",
-          }}
-        >
-          <p style={{ fontWeight: "bold" }}>Customer Type:</p>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Public Sector
+      <div style={{ marginTop: "4rem", border: "1px solid black" }}>
+        <div className="row mt-3">
+          <div className="col-1">
+            <p style={{ fontWeight: "bold", fontSize: "12px" }}>
+              Customer Type:
             </p>
           </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Private Sector
-            </p>
-          </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Industrial Sector
-            </p>
-          </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>Armed Forces</p>
+          <div className="col-11">
+            <div className="row">
+              <div className="col">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    // marginTop: "1rem",
+                  }}
+                >
+                  {/* <p style={{ fontWeight: "bold", fontSize: "12px" }}>
+                    Inquirer:
+                  </p> */}
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Public Sector
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Private Sector
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Industrial Sector
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Armed Forces
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-2">
-            <p style={{ fontWeight: "bold" }}>Inquirer:</p>
+        <div className="row mt-2" style={{ borderBottom: "1px solid black" }}>
+          <div className="col-1">
+            <p style={{ fontWeight: "bold", fontSize: "12px" }}>Inquirer:</p>
           </div>
-          <div className="col-2">
-              
+          <div className="col-11">
+            <div className="row">
+              <div className="col">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    // marginTop: "1rem",
+                  }}
+                >
+                  {/* <p style={{ fontWeight: "bold", fontSize: "12px" }}>
+                    Inquirer:
+                  </p> */}
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Dealer/Distributor
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Project/Reidence Owner
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Architect/Consultant
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Contractor/Builder
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-around",
+                    // marginTop: "1rem",
+                  }}
+                >
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      End User
+                    </p>
+                  </div>
+                  <div style={{ display: "flex", gap: ".5rem" }}>
+                    <p
+                      style={{ border: "1px solid black", padding: ".6rem" }}
+                    ></p>
+                    <p style={{ marginTop: ".3rem", fontSize: "11px" }}>
+                      Other
+                    </p>
+                    <hr
+                      style={{
+                        borderTop: "1px solid black",
+                        padding: "1px",
+                        width: "200px",
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            marginTop: "1rem",
-          }}
-        >
-          <p style={{ fontWeight: "bold" }}>Inquirer:</p>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Dealer/Distributor
+        <div style={{ border: "1px solid black" }}>
+          <div
+            style={{
+              display: "flex",
+              gap: ".5rem",
+              marginTop: "1rem",
+              marginLeft: ".2rem",
+              fontSize: "12px",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>CUSTOMER NAME:</p>
+            <p style={{ textDecoration: "underline" }}>Mr Arsalan Ahmed Khan</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: ".5rem",
+              marginLeft: ".2rem",
+              fontSize: "12px",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>CONTACT PERSON:</p>
+            <p style={{ textDecoration: "underline" }}>Mr Arsalan Ahmed Khan</p>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div
+              style={{
+                display: "flex",
+                gap: ".5rem",
+                marginLeft: ".2rem",
+                fontSize: "12px",
+              }}
+            >
+              <p style={{ fontWeight: "bold" }}>ADDRESS:</p>
+              <p style={{ textDecoration: "underline" }}>
+                Mr Arsalan Ahmed Khan
+              </p>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                gap: ".5rem",
+                marginLeft: ".2rem",
+                fontSize: "12px",
+              }}
+            >
+              <p style={{ fontWeight: "bold" }}>PHONE NO:</p>
+              <p style={{ textDecoration: "underline" }}>
+                234234203423423402304230422342342342354235
+              </p>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: ".5rem",
+              marginLeft: ".2rem",
+              fontSize: "12px",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>SUPPLIED THROUGH:</p>
+            <p style={{ textDecoration: "underline" }}>Mr Arsalan Ahmed Khan</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: ".5rem",
+              marginLeft: ".2rem",
+              fontSize: "12px",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>
+              PROJECT(Mention Name & Address of Site):
+            </p>
+            <p style={{ textDecoration: "underline" }}>Mr Arsalan Ahmed Khan</p>
+          </div>
+        </div>
+        <div style={{ marginTop: "1rem" }}>
+          <div style={{ display: "flex", gap: "1rem" }}>
+            <div
+              style={{
+                display: "flex",
+                gap: ".5rem",
+                marginLeft: ".2rem",
+                fontSize: "12px",
+              }}
+            >
+              <p style={{ fontWeight: "bold" }}>COMPLAINT RECEIVING DATE:</p>
+              <p style={{ textDecoration: "underline" }}>
+                Mr Arsalan Ahmed Khan
+              </p>
+            </div>
+            <p style={{ fontSize: "11px" }}>
+              (attatch letter of customer, where applicable)
             </p>
           </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Project/Reidence Owner
-            </p>
+          <div
+            style={{
+              display: "flex",
+              gap: ".5rem",
+              marginLeft: ".2rem",
+              fontSize: "12px",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>PRODUCT:</p>
+            <p style={{ textDecoration: "underline" }}>Mr Arsalan Ahmed Khan</p>
           </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Architect/Consultant
-            </p>
-          </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>
-              Contractor/Builder
-            </p>
-          </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>End User</p>
-          </div>
-          <div style={{ display: "flex", gap: ".5rem" }}>
-            <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
-            <p style={{ marginTop: ".3rem", fontSize: "12px" }}>Other</p>
+          <div
+            style={{
+              display: "flex",
+              gap: ".5rem",
+              marginLeft: ".2rem",
+              fontSize: "12px",
+            }}
+          >
+            <p style={{ fontWeight: "bold" }}>STAMPING:</p>
+            <div style={{ display: "flex", gap: ".3rem" }}>
+              <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
+              <p>No</p>
+            </div>
+            <div style={{ display: "flex", gap: ".3rem" }}>
+              <p style={{ border: "1px solid black", padding: ".6rem" }}></p>
+              <p>Yes(give details)</p>
+            </div>
             <hr
               style={{
                 borderTop: "1px solid black",
